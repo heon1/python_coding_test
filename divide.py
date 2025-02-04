5c1de7a8843a268d2b22ec8eb2a90be8f8ecfc42
Sangheon Lee <codingheon0129@gmail.com>
2025-02-04 11:42:37 +0900

    N.1008 #1 &  N.1001, N.10998, # 2(주석 수정)

--- divide.py
+++ divide.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     a, b = map(int, input().split())  # 두 숫자를 정수로 받아온다.
     if (0<a) and (a<10) and (0<b) and (b<10):  # 정해진 조건에서 a, b 를 받을 경우 
-        print(a * b)  # 두 변수를 곱해서 출력
+        print(a / b)  # 두 변수를 나눠서 출력
     else:
         pass
